5e48b462a10d3fb9c863c67ace8977e1923a65a6
denizhanpak <dpak1@vols.utk.edu>
2017-07-25 17:42:23 -0400

    Notes for July 25, 2017

--- Denizhan.Pak/Scripts/Data_Editing/AddPosition.py
+++ Denizhan.Pak/Scripts/Data_Editing/AddPosition.py
@@ -3,19 +3,64 @@ import csv
 import random
 
 class Gene:
-    def __init__(self, name, mean, median, ci25, ci975):
+    def __init__(self, name):
         self.id = name
-        self.codons = []
+        self.codons = dict()
+        self.positions = []
+        self.rfp_counts = []
+        self.total = 0
+
+class Codon:
+    def __init__(self, cod, count, rfp):
+        self.codon = cod
+        self.count = int(count)
+        self.rfpcount = int(rfp)
         self.positions = []
 
-class Position:
-    def __init__(self, numpos, cod, gen, rfp):
-        self.pos = numpos
-        self.codon = cod
-        self.gene = gen
-        self.rfpcount = rfp
+with open(sys.argv[1]) as fdata:
+    genes = dict()
+    codons = set()
+    fdata.readline()
+    data = csv.reader(fdata, delimiter = ",")
 
-with open(sys.argv[1]) as data:
-    genes = set()
-    sequence = []
+    for line in data:
+        name = line[0]
+        if name not in genes:
+            genes[name] = Gene(name)
+        g = genes[name]
+        g.codons[line[3]] = Codon(line[3], line[2], line[1])
+        codons.add(line[3])
+        g.total += int(line[2])
 
+for g in genes:
+    gene = genes[g]
+    for i in range(gene.total):
+        c = random.choice(list(codons))
+        codon = gene.codons[c]
+        while c.count < 1:
+            c = random.choice(list(codons))
+            codon = gene.codons[c]
+        codon.count -= 1
+        codon.positions.append(i)
+        gene.positions.append(codon)
+        gene.rfp_counts.append(0)
+
+for g in genes:
+    gene = genes[g]
+    for name in codons:
+        codon = gene.codons[name]
+        if len(codon.positions) == 1:
+            gene.rfp_counts[codon.positions[0]] = codon.rfpcount
+        elif len(codon.positions) > 0:
+            while codon.rfpcount > 0:
+                index = codon.positions[random.randint(0, len(codon.positions) - 1)]
+                rfp = random.randint(0, codon.rfpcount)
+                gene.rfp_counts[index] += rfp
+                codon.rfpcount -= rfp
+
+for g in genes:
+    i = 0
+    gene = genes[g]
+    for pos in gene.positions:
+        print gene.id + ", " + str(i) + ", " + str(pos.codon) + ", " + str(gene.rfp_counts[i])
+        i += 1
